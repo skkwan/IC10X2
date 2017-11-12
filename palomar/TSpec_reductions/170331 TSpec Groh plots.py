388bc0ccf60173818e42c701f585b985c81dc043
Stephanie Kwan <stephaniekwan@Stephanies-MacBook-Pro.local>
2017-11-12 15:35:21 -0800

    Fixed inexplicable 0.0005 micron offset

--- palomar/TSpec_reductions/170331 TSpec Groh plots.py
+++ palomar/TSpec_reductions/170331 TSpec Groh plots.py
@@ -21,22 +21,24 @@ reference spectra to plot.
 
 import numpy as np
 import matplotlib.pyplot as plt
+from matplotlib import rc
 
-# Use LaTeX font
+
+plt.rc('text', usetex=True)
+plt.rc('font', **{'family': 'serif', 'serif': ['Computer Modern']})
+
 plt.rc({'weight' : 'normal',
         'size'   : 15})
-plt.rc('font', **{'family': 'serif', 'serif': ['Computer Modern']})
-#plt.rc('font',**{'family':'sans-serif','sans-serif':['Helvetica']})
-plt.rc('text', usetex = True)
+
 
 plt.clf()
 plt.close()
 table = np.genfromtxt('IC10X2_JHK_modified.rtf', delimiter = '    ', 
                   comments = '\p', skip_header = 2, skip_footer = 4)
-wl    = table[:, 0] - 0.0005
+wl    = table[:, 0] 
 counts = table[:, 1]
 fig = plt.figure()
-normFlux = counts / 0.024               # NOrmalize to height of He I being 1
+normFlux = counts / 0.024               # Normalize to height of He I being 1
 
 lbv1_table = np.genfromtxt('groh_data/w243.txt', delimiter = ' ', 
                   comments = '#', skip_header = 1)
@@ -91,33 +93,30 @@ be3_wl = be3_table[:, 0] * 10**(-4)
 be3_normFlux = be3_table[:, 2]                          
 
  # Two subplots, unpack the axes array immediately
-f, ((ax1, ax2), (ax3, ax4), (ax5, ax6)) =   \
-     plt.subplots(3, 2) 
+f, ((ax1, ax2), (ax3, ax4), (ax5, ax6)) = plt.subplots(3, 2) 
 axlargest = plt.gca()
 
-
+###########################################
+# IC10X2 plots
+###########################################
+ax1.set_title('IC 10 X-2', fontsize = 14)
+ax1.text(0.03, 1.3, '\\textbf{(a)}', transform=ax1.transAxes,
+      fontsize = 14, fontweight = 'bold', va = 'top', ha = 'right')
+# Plot IC10X2 spectra
 ax1.plot(wl[7100:7500], normFlux[7100:7500], color = 'black')
 ax1.invert_xaxis()
 ax1.set_xlim([1.075, 1.100])
 ax1.set_ylim([0, 1.9])
 
 # Plot and label the original He I line in red
-ax1.axvline(x = 1.08303398 - 0.0005, color = 'red', ls = 'dashed')
-#ax1.text(1.084- 0.0005, 1.5, 'He I (shifted)', color = 'red', rotation = 90, 
-#         fontsize = 12)
-# Plot and label the peak emission line in green
-ax1.axvline(x = 1.08239- 0.0005, color = 'green', ls = 'dashed')
+ax1.axvline(x = 1.08303398, color = 'red', ls = 'dashed')
+# Plot and label the peak He I emission line in green 1.08239
+ax1.axvline(x = 1.08236, color = 'green', ls = 'dashed')
 ax1.text(1.08, 1.2, 'He I', color = 'green', rotation = 90, fontsize = 12)
 # Paschen-gamma lines
-ax1.axvline(x = 1.093817- 0.0005, color = 'red', ls = 'dashed')
-#ax1.text(1.095- 0.0005, 1.5, 'Pa$\gamma$', rotation = 90, 
-#         fontsize = 12, color = 'red')
-ax1.axvline(x = 1.0931- 0.0005, color = 'green', ls = 'dashed')
+ax1.axvline(x = 1.093817, color = 'red', ls = 'dashed')
+ax1.axvline(x = 1.0931, color = 'green', ls = 'dashed')
 ax1.text(1.091, 1.2, 'Pa $\gamma$', rotation = 90, fontsize = 12, color = 'green') 
-ax1.set_title('IC 10 X-2', fontsize = 14)
-
-ax1.text(0.03, 1.3, '\\textbf{(a)}', transform=ax1.transAxes,
-      fontsize = 14, fontweight = 'bold', va = 'top', ha = 'right')
 
 
 # Remove walls and ticks between the IC 10 X-2 plots
@@ -138,11 +137,11 @@ ax2.plot((-d,d),(1-d,1+d), **kwargs) # bottom-right diagonal
 # Paschen-beta lines
 # Plot the original emission line in red
 ax2.plot(wl[5200:5389], normFlux[5200:5389], color = 'black')
-ax2.axvline(x = 1.282- 0.0005, color = 'red', ls = 'dashed')
+ax2.axvline(x = 1.282, color = 'red', ls = 'dashed')
 #ax2.text(1.283- 0.0005, 1.5, 'Pa $\\beta$ (1.2815)', rotation = 90, 
 #         fontsize = 12, color = 'red')
 # Plot the peak emission line in green
-ax2.axvline(x = 1.28103- 0.0005, color = 'green', ls = 'dashed')
+ax2.axvline(x = 1.28103, color = 'green', ls = 'dashed')
 ax2.text(1.2786, 1.2, 'Pa $\\beta$', rotation = 90,
          fontsize = 12, color = 'green')
 ax2.invert_xaxis()
@@ -151,14 +150,16 @@ ax2.set_ylim([0, 1.9])
 
 offset = 0.000717
 
+###########################################
 # LBV/LBV candidate plot
+###########################################
 min2, max2   = 570, 570 + 394
-ax3.plot(lbv1_wl[min2:max2], lbv1_normFlux[min2:max2],
+ax3.plot(lbv1_wl[min2:max2], lbv1_normFlux[min2:max2]  + 0.4,
          color = 'grey', linewidth = 0.75)
-ax3.plot(lbv2_wl[434:828], lbv2_normFlux[434:828] + 0.7, '-',
+ax3.plot(lbv2_wl[434:828], lbv2_normFlux[434:828] + 0.4, '-',
          color = 'grey', dashes= (2, 0.4), linewidth = 0.5)
 ax3.plot(lbv3_wl[441:835], lbv3_normFlux[441:835] + 1, color = 'black', linewidth = 0.5)
-ax3.plot(wl[7100:7500] + offset,
+ax3.plot(wl[7100:7500] + 0.00064397,
          normFlux[7100:7500], color = 'black') # IC 10 X-2
 ax3.set_yticks(np.arange(0.0, 2.0, 0.5))
 ax3.set_xlim([1.075, 1.100])
@@ -223,4 +224,4 @@ f.text(0.05, 0.5, 'Flux (arbitrary units)', ha = 'center',
 plt.subplots_adjust(wspace = 0.2, hspace= 0.7)
 #plt.tight_layout()
  
-plt.savefig('170521 TSpec and Groh plot.pdf')    
+plt.savefig('171111 TSpec and Groh plot.pdf')    
